3697ef340d08c951ae540a3c90745f4bf042e117
MrFang <bat312@yandex.ru>
2022-02-20 16:08:18 +0300

    Add Jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
     kotlin("jvm") version "1.6.10"
     application
+    jacoco
 }
 
 group = "me.fang"
@@ -19,6 +20,17 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        xml.outputLocation.set(File("$buildDir/reports/jacoco/report.xml"))
+        html.required.set(false)
+        csv.required.set(false)
+    }
+    executionData(File("build/jacoco/test.exec"))
+    dependsOn("test")
 }
 
 tasks.withType<KotlinCompile>() {
