788300e565304c5eb5df8d727a98d26891542df6
MrFang <bat312@yandex.ru>
2022-02-23 21:38:37 +0300

    Var assigment test

--- src/main/kotlin/me/fang/kosh/process/VariableAssigment.kt
+++ src/main/kotlin/me/fang/kosh/process/VariableAssigment.kt
@@ -4,12 +4,10 @@ import me.fang.kosh.Environment
 
 class VariableAssigment(override val args: List<String>) : KoshProcess {
     override fun run(stdin: String): String {
-        val assigment = args[0].split(' ', limit = 1)
+        val assigment = args[0].split("=", limit = 2)
         Environment.vars[assigment[0]] = assigment[1]
 
-        if (args.size > 1) {
-            return processSingleCommand(args.drop(1))
-        }
+        if (args.size > 1) return processSingleCommand(args.drop(1))
 
         return ""
     }
---
+++ src/test/kotlin/me/fang/kosh/process/VariableAssigmentTest.kt
@@ -0,0 +1,14 @@
+package me.fang.kosh.process
+
+import me.fang.kosh.Environment
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class VariableAssigmentTest {
+    @Test
+    fun assigmentTest() {
+        assertEquals("123", VariableAssigment(listOf("X=2", "echo", "-n", "123")).run())
+        assertEquals("2", Environment.vars["X"])
+        assertEquals("", VariableAssigment(listOf("X=2")).run())
+    }
+}
